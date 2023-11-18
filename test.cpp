0c0ce22ad79baa6c42968e5f0c485bdb83cfd9e2
= <muhbintang650@gmail.com>
2023-11-19 07:10:31 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int tambah(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    int hasil = tambah(1, 2);
+
+    cout << "hasil : " << hasil << endl;
+    return 0;
+}
