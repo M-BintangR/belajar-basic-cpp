a82b206e8cf2716d3518936f3adff9ae49e58ac2
= <muhbintang650@gmail.com>
2023-11-20 21:00:18 +0800

    if statement cpp ✅

---
+++ if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout << "masukkan angka = ";
+    cin >> a;
+
+    // if statment
+    // kondisi dalam boolean
+    if (a > 3)
+    {
+        cout << "angka lebih besar dari 3" << endl;
+    }
+
+    cout << "selesai" << endl;
+
+    return 0;
+}
