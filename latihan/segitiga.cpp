fd024b0aab45a9c7d0d144679dc783a7185fd96d
= <muhbintang650@gmail.com>
2023-11-21 00:19:50 +0800

    tambah pola segitiga ✅

--- latihan/segitiga.cpp
+++ latihan/segitiga.cpp
@@ -59,5 +59,65 @@ int main()
         cout << endl;
     }
 
+    cout << "pola ke 3 \n";
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = n; k > i; k--)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = n; k > i; k--)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    cout << "pola ke 4 \n";
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = n; k > i; k--)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 1; k < i; k++)
+        {
+            cout << " ";
+        }
+        for (int j = n; j >= (2 * i - n); j--)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
