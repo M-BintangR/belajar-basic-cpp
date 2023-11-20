96b44575a12190a02468911c4307b2ae13fce811
= <muhbintang650@gmail.com>
2023-11-20 22:25:43 +0800

    while loop cpp ✅

---
+++ whileLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+
+    while (a <= 10)
+    {
+        cout << "perulangan yang ke : " << a << endl;
+        a++;
+    }
+
+    return 0;
+}
