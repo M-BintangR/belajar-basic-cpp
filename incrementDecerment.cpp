19f365a51c42cc9b89fcd745ec8a90c4c5aeb357
= <muhbintang650@gmail.com>
2023-11-20 22:19:34 +0800

    increment & decerment cpp ✅

---
+++ incrementDecerment.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 10;
+
+    // INCREMENT
+    cout << "sebelum increment : " << a << endl;
+    a++;
+    cout << "sesudah increment : " << a << endl;
+
+    // DECEMENT
+    cout << "sebelum decerment : " << b << endl;
+    b--;
+    cout << "sesudah decerment : " << b << endl;
+
+    return 0;
+}
