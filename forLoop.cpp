a6fc516460b0de9375c30b4e2f1510daf341df6d
= <muhbintang650@gmail.com>
2023-11-20 23:26:22 +0800

    for loop cpp ✅

---
+++ forLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // FOR LOOP
+
+    for (int a = 1; a <= 10; a++)
+    {
+        cout << "perulangan yang ke : " << a << endl;
+    }
+
+    return 0;
+}
