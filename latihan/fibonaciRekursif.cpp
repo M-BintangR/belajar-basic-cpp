58a47813788e79f4348587b7ae6fbe9db34f52b7
= <muhbintang650@gmail.com>
2023-11-21 21:27:58 +0800

    latihan fibonaci dengan rekursif ✅

---
+++ latihan/fibonaciRekursif.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonaci(int n);
+
+int main()
+{
+    int angka, hasil;
+
+    cout << "Masukkan angka fibonaci : ";
+    cin >> angka;
+
+    hasil = fibonaci(angka);
+
+    cout << "Hasil fibonaci adalah   : " << hasil << endl;
+
+    return 0;
+}
+
+int fibonaci(int n)
+{
+    if ((n == 0) || (n == 1))
+    {
+        return n;
+    }
+    else
+    {
+        return fibonaci(n - 1) + fibonaci(n - 2);
+    }
+}
